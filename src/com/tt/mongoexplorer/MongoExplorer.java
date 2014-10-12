548f2a13c9f0584c41bc3a7c7097b057735cd7ca
Tuomas Tikka <tuomas.tikka@gmail.com>
2014-10-12 23:16:20 +0300

    remove logging for now

--- src/com/tt/mongoexplorer/MongoExplorer.java
+++ src/com/tt/mongoexplorer/MongoExplorer.java
@@ -3,15 +3,10 @@ package com.tt.mongoexplorer;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.tt.mongoexplorer.view.MainFrame;
 
 public class MongoExplorer {
 
-	static Logger log = LogManager.getLogger(MongoExplorer.class.getName());
-	
 	public static void main(String[] args) {
 		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		Runnable ui = new Runnable() {
