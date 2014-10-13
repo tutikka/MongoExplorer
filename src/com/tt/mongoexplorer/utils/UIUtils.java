0cad88600e84ee9f3ed43634cd40bc18bf403ffd
Tuomas Tikka <tuomas.tikka@gmail.com>
2014-10-13 21:34:50 +0300

    Add method to load icons

--- src/com/tt/mongoexplorer/utils/UIUtils.java
+++ src/com/tt/mongoexplorer/utils/UIUtils.java
@@ -1,18 +1,29 @@
 package com.tt.mongoexplorer.utils;
 
 import java.awt.Component;
+import java.io.File;
 
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class UIUtils {
 
 	public static void info(Component parent, String message) {
-		JOptionPane.showMessageDialog(parent, message, "Heads Up!", JOptionPane.INFORMATION_MESSAGE, new ImageIcon("resources/large/info.png"));
+		JOptionPane.showMessageDialog(parent, message, "Heads Up!", JOptionPane.INFORMATION_MESSAGE, UIUtils.icon("resources/large/info.png"));
 	}
 	
 	public static void error(Component parent, String message) {
-		JOptionPane.showMessageDialog(parent, message, "Oops!", JOptionPane.ERROR_MESSAGE, new ImageIcon("resources/large/error.png"));
+		JOptionPane.showMessageDialog(parent, message, "Oops!", JOptionPane.ERROR_MESSAGE, UIUtils.icon("resources/large/error.png"));
+	}
+	
+	public static Icon icon(String path) {
+		File file = new File(path);
+		if (file.exists()) {
+			return (new ImageIcon(path));
+		} else {
+			return (new ImageIcon(UIUtils.class.getResource("/" + path)));
+		}
 	}
 	
 }
