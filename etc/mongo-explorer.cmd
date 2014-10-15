1dcd8a3ba79f7488c57c71cce844060269543598
tikkatu <tuomas.tikka@bcbmedical.com>
2014-10-15 16:23:06 +0300

    Add gson to startup scripts

--- etc/mongo-explorer.cmd
+++ etc/mongo-explorer.cmd
@@ -1,1 +1,1 @@
-java -cp libs/mongo-explorer.jar;libs/mongo-java-driver-2.12.3.jar com.tt.mongoexplorer.MongoExplorer
+java -cp libs/mongo-explorer.jar;libs/mongo-java-driver-2.12.3.jar;libs/gson-2.3.jar com.tt.mongoexplorer.MongoExplorer
